d12c774028d2ae0762c2d9750f1b515eb5f3f633
Nishant Kumar Rai <66172201+nishantkumarai@users.noreply.github.com>
2025-02-10 04:42:07 +0530

    Implementing Service Locator

---
+++ Assets/Scripts/Main/GameService.cs
@@ -0,0 +1,23 @@
+using ServiceLocator.Player;
+using ServiceLocator.Utilities;
+using UnityEngine;
+
+
+public class GameService : GenericMonoSingleton<GameService>
+{
+    public PlayerService playerService {  get; private set; }
+
+
+    [SerializeField] public PlayerScriptableObject playerScriptableObject;
+
+    private void Start()
+    {
+        playerService = new PlayerService(playerScriptableObject);
+    }
+
+    private void Update()
+    {
+        playerService.Update();
+    }
+
+}

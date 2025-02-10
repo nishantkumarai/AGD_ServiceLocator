bfda9293169d48a98d5a8dd7685a2d09c1be8d8a
Nishant Kumar Rai <66172201+nishantkumarai@users.noreply.github.com>
2025-02-10 06:06:23 +0530

    PlayerService

--- Assets/Scripts/Main/GameService.cs
+++ Assets/Scripts/Main/GameService.cs
@@ -1,18 +1,49 @@
+using ServiceLocator.Events;
+using ServiceLocator.Map;
 using ServiceLocator.Player;
+using ServiceLocator.Sound;
+using ServiceLocator.UI;
 using ServiceLocator.Utilities;
+using ServiceLocator.Wave;
 using UnityEngine;
 
 
 public class GameService : GenericMonoSingleton<GameService>
 {
     public PlayerService playerService {  get; private set; }
+    public SoundService soundService { get; private set; }
+    public MapService mapService { get; private set; }
+    public WaveService waveService { get; private set; }
+    public EventService eventService { get; private set; }
 
 
-    [SerializeField] public PlayerScriptableObject playerScriptableObject;
+    [SerializeField] private UIService uIService;
+    public UIService UiService => uIService;
+
+    //[SerializeField] private EventService eventService;
+    //public EventService EventService => eventService;
+
+
+    [SerializeField] private PlayerScriptableObject playerScriptableObject;
+    [SerializeField] private SoundScriptableObject soundScriptableObject;
+    [SerializeField] private MapScriptableObject mapScriptableObject;
+    [SerializeField] private WaveScriptableObject waveScriptableObject;
+
+
+    [SerializeField] private AudioSource audioEffects;
+    [SerializeField] private AudioSource backgroundMusic;
+
+
+    
 
     private void Start()
     {
+        eventService = new EventService();
+        UiService.SubscribeToEvents();
         playerService = new PlayerService(playerScriptableObject);
+        soundService = new SoundService(soundScriptableObject, audioEffects, backgroundMusic);
+        mapService = new MapService(mapScriptableObject);
+        waveService = new WaveService(waveScriptableObject);
     }
 
     private void Update()
